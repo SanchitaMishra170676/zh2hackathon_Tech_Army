f66a209a8a4c2952b9c17f44938e03e32b6eb171
Sanchita Mishra <59258203+SanchitaMishra170676@users.noreply.github.com>
2021-08-26 17:49:31 +0530

    [Fixes: #1] create acc added

--- home/views.py
+++ home/views.py
@@ -9,9 +9,6 @@ def home(request):
 def login(request):    
     return render(request,'login.html')
 
-def signup(request):
-    return render(request,'signup.html')
-
 def dashboard(request):
     return render(request,'index.html')
 
@@ -20,3 +17,33 @@ def contact(request):
 
 def underprogress(request):
     return render(request,'underprogress.html')
+
+def createaccount(request):
+    if request.method == "POST":
+        try:
+            Salutation = request.POST['salutation']
+            FirstName = request.POST['first-name']
+            MiddleName = request.POST['middle-name']
+            LastName = request.POST['last-name']
+            Gender = request.POST['gender']
+            MothersMaidenName = request.POST['mmn']
+            Aadhar = request.POST['aadhar']
+            Email = request.POST['email']
+            Phone = request.POST['phone']
+            Password= request.POST['password']
+            try:
+                Dob = request.POST['dob']
+            except:
+                Dob= "28-09-2001"
+            try:
+                ProfilePic=  request.FILES['profilepic']
+            except:
+                pass      
+            messages.success(request,"Account Added Successfully")
+            # return render(request,'index.html')
+            return redirect('dashboard')
+        except:
+            messages.error(request,"Can not create account, Kindly reach out to us on customer services.")
+            return redirect('home')
+
+    return render(request,'createacc.html')
