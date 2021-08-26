3d954e6dca5062de01031c96833235d043e0c6d4
Sanchita Mishra <59258203+SanchitaMishra170676@users.noreply.github.com>
2021-08-26 19:52:10 +0530

    authentication added

--- home/models.py
+++ home/models.py
@@ -1,3 +1,5 @@
 from django.db import models
-
+from django.utils import timezone
 # Create your models here.
+
+
--- home/views.py
+++ home/views.py
@@ -1,13 +1,19 @@
-from django.shortcuts import render, redirect 
+from django.contrib.auth import logout as auth_logout, login as auth_login
+from django.shortcuts import render, redirect,HttpResponse,Http404
 from django.contrib import messages
+from django.contrib.auth import authenticate
+from django.contrib.auth.models import User
+from django.template.loader import render_to_string
+from django.utils.functional import LazyObject
+
 
 # Create your views here.
 
 def home(request):
     return render(request,'login.html') 
   
-def login(request):    
-    return render(request,'login.html')
+# def login(request):    
+#     return render(request,'login.html')
 
 def dashboard(request):
     return render(request,'index.html')
@@ -17,6 +23,34 @@ def contact(request):
 
 def underprogress(request):
     return render(request,'underprogress.html')
+
+def login(request):
+    if request.method == 'POST':
+        loginusername = request.POST['username']
+        loginpassword=request.POST['password']
+
+        user = authenticate(username =loginusername,password =loginpassword)
+
+        if user is not None:
+            auth_login(request,user)
+            messages.success(request,'Logged in Successfully')
+            return redirect ('dashboard')
+        
+        else:
+            messages.error(request,"Invalid Credentials")
+            return redirect('login')
+
+    return render(request,'login.html')
+
+
+def logout(request):
+    if request.user.is_authenticated:
+        auth_logout(request)
+        messages.success(request,'successfully logged out')
+        return redirect('home')
+    else:
+        return HttpResponse('404 Not found')
+
 
 def createaccount(request):
     if request.method == "POST":
@@ -39,9 +73,14 @@ def createaccount(request):
                 ProfilePic=  request.FILES['profilepic']
             except:
                 pass      
+
+            user = User.objects.create_user(username=Email, password=Password)
+            user.first_name = FirstName
+            user.last_name = LastName
+            # user.form_id = 
             messages.success(request,"Account Added Successfully")
             # return render(request,'index.html')
-            return redirect('dashboard')
+            return redirect('login')
         except:
             messages.error(request,"Can not create account, Kindly reach out to us on customer services.")
             return redirect('home')
